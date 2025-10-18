9d3a3b743211040043b11db98a65cb52f47c5c66
Irina Shin <flackylovescats@gmail.com>
2025-10-18 17:02:23 +0300

    fix python lab1

--- Python/lab1/two_sum.py
+++ Python/lab1/two_sum.py
@@ -2,5 +2,5 @@ def two_sum(nums, target):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
-                return [i, j]
-    return [-1] * 2
+                return i, j
+    return -1, -1
